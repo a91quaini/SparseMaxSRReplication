9147025fe43052e61b2666b44febd59383921030
a91quaini <alberto91quaini@gmail.com>
2025-05-28 09:54:52 +0200

    package is born

--- src/SparseMaxSRReplication.jl
+++ src/SparseMaxSRReplication.jl
@@ -1,5 +1,21 @@
 module SparseMaxSRReplication
 
-greet() = print("Hello World!")
+using Serialization
 
-end # module SparseMaxSRReplication
+const DATADIR = joinpath(@__DIR__, "..", "data")
+
+"""
+    load_matrix(name::String) -> Matrix{Float64}
+
+Load the serialized Matrix stored in `data/<name>.jls`.
+"""
+function load_matrix(name::AbstractString)
+    path = joinpath(DATADIR, name*".jls")
+    open(path) do io
+        return deserialize(io)
+    end
+end
+
+export load_matrix
+
+end # module
